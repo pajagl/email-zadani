f9fc96b92ce3a0e351f1d680a602af3da3c98904
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-09 19:19:32 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('funguju');
+const goodbye = (name) => {
+	return 'S pozdravem ' + name
+}
+
+const formalGoodbye = (name) => {
+	return 'S uctivou poklonou ' + name
+}
+
+const rudeGoodbye = (name) => {
+	return 'Čauky mňauky. ' + name
+}
+
+const fillSubject = (subject) => {
+	document.querySelector('.email__subject').textContent = subject
+}
+
+const fillBody = (body, name, goodbyeFunction) => {
+	const bodyElm = document.querySelector('.email__body')
+	bodyElm.innerHTML = body
+	const closingElm = document.querySelector('.email__closing')
+	closingElm.textContent = goodbyeFunction(name)
+}
+
+fillSubject('Pozvánka na oslavu narozenin')
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye)
